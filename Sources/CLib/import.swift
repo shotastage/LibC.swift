696b111bce3f1727d4781eb37b262aa035afaf14
shotastage <shota.shimazu@magicalsoft.app>
2025-02-13 14:25:49 +0900

    Refactor import statements for cross-platform compatibility in import.swift

--- Sources/CLib/import.swift
+++ Sources/CLib/import.swift
@@ -2,11 +2,8 @@
 // Copyright (c) 2016-2025 Shota Shimazu
 // This program is freely distributed under the MIT, see LICENSE for detail.
 
-
-#if os(Linux)
-    // Import Glibc on Linux
+#if canImport(Glibc)
     @_exported import Glibc
-#elseif os(OSX)
-    // Import Darwin on macOS
+#elseif canImport(Darwin.C)
     @_exported import Darwin.C
 #endif
